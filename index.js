e5c264f8acb7e60847957c129fd18c214bcd7315
Sammy <sammy.cullinan@gmail.com>
2020-08-02 13:25:26 -0700

    initial file setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+
+
