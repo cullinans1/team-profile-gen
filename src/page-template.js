aa362d257ddc1583834b9aad1d37c761b95ded19
Sammy <sammy.cullinan@gmail.com>
2020-08-03 18:03:51 -0700

    finished styling

--- src/page-template.js
+++ src/page-template.js
@@ -12,7 +12,7 @@ const generateTeam = team => {
             <div class='card-body'>
                 <ul class='list-group'>
                     <li class='list-group-item'>ID: ${manager.getId()}</li>
-                    <li class='list-group-item'>Email: ${manager.getEmail()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
                     <li class='list-group-item'>Office Number: ${manager.getOfficeNumber()}</li>
                 </ul>
             </div>
@@ -29,7 +29,7 @@ const generateTeam = team => {
             <div class='card-body'>
                 <ul class='list-group'>
                     <li class='list-group-item'>ID: ${engineer.getId()}</li>
-                    <li class='list-group-item'>Email: ${engineer.getEmail()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
                     <li class='list-group-item'>GitHub: <a href='https://github.com/${engineer.getGithub()}' target='_blank'>${engineer.getGithub()}</a></li>
                 </ul>
             </div>
@@ -46,7 +46,7 @@ const generateTeam = team => {
             <div class='card-body'>
                 <ul class='list-group'>
                     <li class='list-group-item'>ID: ${intern.getId()}</li>
-                    <li class='list-group-item'>Email: ${intern.getEmail()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
                     <li class='list-group-item'>School: ${intern.getSchool()}</li>
                 </ul>
             </div>
